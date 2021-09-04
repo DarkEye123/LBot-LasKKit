3bba2eb4f9f985cc4898241b3819fbf713ad99c8
Matej Lesko <lesko.matej.pu@gmail.com>
2021-09-04 17:13:38 +0200

    renaming according to styleguide

--- lib/integrated_led/integrated_led.cc
+++ lib/integrated_led/integrated_led.cc
@@ -8,7 +8,7 @@ namespace integrated_led
         pinMode(LED_BUILTIN, OUTPUT);
     }
 
-    void Blink(bool serial_output_enabled = false, uint16_t waitFor = 500)
+    void Blink(bool serial_output_enabled = false, uint16_t wait_for = 500)
     {
         // turn the LED on (HIGH is the voltage level)
         if (serial_output_enabled)
@@ -19,7 +19,7 @@ namespace integrated_led
         digitalWrite(LED_BUILTIN, HIGH);
 
         // wait for a second
-        delay(waitFor);
+        delay(wait_for);
 
         // turn the LED off by making the voltage LOW
         if (serial_output_enabled)
@@ -28,6 +28,6 @@ namespace integrated_led
             Serial.println("Low");
         }
         digitalWrite(LED_BUILTIN, LOW);
-        delay(waitFor);
+        delay(wait_for);
     }
 }
--- lib/integrated_led/integrated_led.h
+++ lib/integrated_led/integrated_led.h
@@ -1,5 +1,5 @@
 namespace integrated_led
 {
-    void Blink(bool serial_output_enabled = false, uint16_t waitFor = 500);
+    void Blink(bool serial_output_enabled = false, uint16_t wait_for = 500);
     void Setup();
 }

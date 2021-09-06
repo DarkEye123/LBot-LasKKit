8846f1b36db15a9cbe4ee72fd9e693b17aba8be0
Matej Lesko <lesko.matej.pu@gmail.com>
2021-09-06 22:30:46 +0200

    call of ir receiver + blink usage removal

--- src/main.cpp.ino
+++ src/main.cpp.ino
@@ -3,6 +3,7 @@
 #include "../lib/intelligent_led/intelligent_led.h"
 #include "../lib/bell/bell.h"
 #include "../lib/button/button.h"
+#include "../lib/ir_receiver/ir_receiver.h"
 
 #include "Arduino.h"
 
@@ -10,25 +11,25 @@ void setup()
 {
   Serial.begin(9600);
 
-  integrated_led::Setup();
-
+  // integrated_led::Setup();
   intelligent_led::Setup();
-
   button::Setup();
 
   bell::Setup();
   bell::Buzz();
+
+  ir_receiver::Setup();
 }
 
 void loop()
 {
-  button::LogValue();
   button::RunOnPress([](void)
                      {
                        Serial.println("just pressing the button");
                        bell::Buzz(100);
+                       light_sensor::LogValue();
                      });
-  light_sensor::LogValue();
-  integrated_led::Blink();
+  // integrated_led::Blink(true, 500); // for now this fights with intelligent leds on PIN 13 --> I need to check registers
   intelligent_led::SetRandomColorOnNextLed();
+  ir_receiver::LogValue();
 }

2f52d5e6bca5ca429d56e69d1f4f7dabe7bfa2cb
Matej Lesko <lesko.matej.pu@gmail.com>
2021-09-09 19:05:10 +0200

    motors lib

--- lib/motors/motors.cc
+++ lib/motors/motors.cc
@@ -1,7 +1,7 @@
 #include "Arduino.h"
+#include "ir_receiver.h"
 
 #include "motors.h"
-#include "ir_receiver.h"
 
 namespace motors
 {
@@ -9,33 +9,55 @@ namespace motors
     {
         pinMode(pwm_pin, OUTPUT);
         pinMode(pin, OUTPUT);
-        this->stop();
+        this->Stop();
     };
 
-    void Motor::stop()
+    void Motor::Stop()
     {
         digitalWrite(this->pin, LOW);
         analogWrite(this->pwm_pin, LOW);
     }
 
-    void Motor::positiveRotation(unsigned short speed)
+    void Motor::PositiveRotation(unsigned short speed)
     {
         digitalWrite(this->pin, LOW);
         analogWrite(this->pwm_pin, speed);
     }
 
-    void Motor::negativeRotation(unsigned short speed)
+    void Motor::NegativeRotation(unsigned short speed)
     {
         digitalWrite(this->pin, HIGH);
         analogWrite(this->pwm_pin, speed);
     }
 
-    MovementManager::MovementManager(Motor *A, Motor *B) : A(A), B(B){};
+    MovementManager::MovementManager(Motor *A, Motor *B) : A(A), B(B), processing_period(200), last_command(0), last_timestamp(millis()){};
 
-    void MovementManager::moveForward(unsigned short speed)
+    void MovementManager::MoveForward(unsigned short speed)
     {
-        this->A->positiveRotation(speed);
-        this->B->positiveRotation(speed);
+        this->A->PositiveRotation(speed);
+        this->B->PositiveRotation(speed);
+    };
+
+    void MovementManager::MoveBackward(unsigned short speed)
+    {
+        this->A->NegativeRotation(speed);
+        this->B->NegativeRotation(speed);
+    };
+
+    void MovementManager::TurnRight(unsigned short speed)
+    {
+        this->A->PositiveRotation(speed);
+    };
+
+    void MovementManager::TurnLeft(unsigned short speed)
+    {
+        this->B->PositiveRotation(speed);
+    };
+
+    void MovementManager::StopMovement(void)
+    {
+        this->A->Stop();
+        this->B->Stop();
     };
 
     MovementManager::~MovementManager()
@@ -51,12 +73,117 @@ namespace motors
         return new MovementManager(A, B);
     };
 
-    void MovementManager::Process(Data *data)
+    boolean MovementManager::IsProcessing()
     {
+        return millis() - this->last_timestamp <= this->processing_period;
+    }
+
+    boolean MovementManager::IsSupportedCmd(unsigned long cmd)
+    {
+        for (unsigned short x = 0; x < MOTORS_SUPPORTED_CMD_LIST_LENGTH; ++x)
+        {
+            if (this->supported_cmds[x] == cmd)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void MovementManager::Process(Data *data, boolean debug, boolean debug_all)
+    {
+        if (debug && debug_all)
+        {
+            Serial.print("MovementManager::Process::data->received ");
+            Serial.println(data->received);
+
+            Serial.print("MovementManager::Process::last_command ");
+            Serial.println(last_command);
+        }
         if (data->received)
         {
-            Serial.print("processing ");
-            Serial.println(data->data, DEC);
+            if (debug)
+            {
+                Serial.print("MovementManager::Process::data->data ");
+                Serial.println(data->data);
+            }
+            if (this->IsSupportedCmd(data->data))
+            {
+                if (data->data == MOTORS_FORWARD)
+                {
+                    if (debug)
+                    {
+                        Serial.println("MovementManager::Process::MoveForward");
+                        Serial.flush();
+                    }
+
+                    MoveForward();
+                    this->last_command = MOVE_FORWARD_CMD;
+                }
+                else if (data->data == MOTORS_BACKWARD)
+                {
+                    if (debug)
+                    {
+                        Serial.println("MovementManager::Process::MoveBackwards");
+                        Serial.flush();
+                    }
+
+                    MoveBackward();
+                    this->last_command = MOVE_BACKWARDS_CMD;
+                }
+                else if (data->data == MOTORS_RIGHT)
+                {
+                    if (debug)
+                    {
+                        Serial.println("MovementManager::Process::TurnRight");
+                        Serial.flush();
+                    }
+
+                    TurnRight();
+                    this->last_command = TURN_RIGHT_CMD;
+                }
+                else if (data->data == MOTORS_LEFT)
+                {
+                    if (debug)
+                    {
+                        Serial.println("MovementManager::Process::TurnLeft");
+                        Serial.flush();
+                    }
+
+                    TurnLeft();
+                    this->last_command = TURN_LEFT_CMD;
+                }
+                this->last_timestamp = millis();
+            }
+            else
+            {
+                const bool is_processing = this->IsProcessing();
+                if (debug)
+                {
+                    Serial.print("MovementManager::Process::last_command ");
+                    Serial.println(last_command);
+                    Serial.print("MovementManager::Process::IsProcessing ");
+                    Serial.println(is_processing);
+                }
+                if (!is_processing)
+                {
+
+                    if (last_command != STOP_CMD)
+                    {
+                        if (debug)
+                            Serial.println("MovementManager::Process::StopMovement");
+                        StopMovement();
+                    }
+                    last_command = STOP_CMD;
+                }
+            }
+        }
+        else if (!this->IsProcessing())
+        {
+            if (debug)
+                Serial.println("MovementManager::Process::StopMovement");
+            StopMovement();
+            last_command = STOP_CMD;
         }
     }
 };
--- lib/motors/motors.h
+++ lib/motors/motors.h
@@ -3,6 +3,18 @@
 
 #include "ir_receiver.h"
 
+#define MOTORS_FORWARD 3877175040
+#define MOTORS_BACKWARD 2907897600
+#define MOTORS_LEFT 4144561920
+#define MOTORS_RIGHT 2774204160
+#define MOTORS_SUPPORTED_CMD_LIST_LENGTH 4
+
+#define STOP_CMD 0
+#define MOVE_FORWARD_CMD 1
+#define MOVE_BACKWARDS_CMD 2
+#define TURN_RIGHT_CMD 3
+#define TURN_LEFT_CMD 4
+
 namespace motors
 {
     class Motor
@@ -12,9 +24,9 @@ namespace motors
 
     public:
         Motor(unsigned short pwm_pin, unsigned short pin);
-        void stop();
-        void positiveRotation(unsigned short speed);
-        void negativeRotation(unsigned short speed);
+        void Stop();
+        void PositiveRotation(unsigned short speed);
+        void NegativeRotation(unsigned short speed);
     };
 
     class MovementManager
@@ -23,11 +35,25 @@ namespace motors
         Motor *A;
         Motor *B;
 
+        unsigned short processing_period;
+        unsigned int last_command;
+
+    private:
+        unsigned long last_timestamp;
+        unsigned long supported_cmds[MOTORS_SUPPORTED_CMD_LIST_LENGTH] = {MOTORS_FORWARD, MOTORS_BACKWARD, MOTORS_LEFT, MOTORS_RIGHT};
+
     public:
         MovementManager(Motor *A, Motor *B);
         ~MovementManager();
-        void moveForward(unsigned short speed = 500); // 500 is ok for usb, not batteries
-        void Process(Data *data);
+        void MoveForward(unsigned short speed = 200); // 500 is ok for usb, not batteries
+        void MoveBackward(unsigned short speed = 200);
+        void TurnRight(unsigned short speed = 200);
+        void TurnLeft(unsigned short speed = 200);
+        // void MoveForward(unsigned short speed = 255); // 255 is ok for batteries
+        void StopMovement(void);
+        void Process(Data *data, boolean debug = false, boolean debug_all = false);
+        boolean IsProcessing();
+        boolean IsSupportedCmd(unsigned long cmd);
     };
 
     MovementManager *Setup(Motor *left = nullptr, Motor *right = nullptr);
